aab7d838afd5e3ac63e8514fcb152e246c382f7f
ahmadabdul592 <ahmadabdul592@gmail.com>
2022-08-24 10:28:34 -0700

    Binary Tree Implementation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,23 +1,21 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the node to create
+ * binary_tree_node - create a binary tree node
+ * @parent: pointer to the parent node
  * @value: value to put in the new node
- *
- * Return: Pointer to the newly created node
- *         NULL on failure
+ * Return: pointer to the newly created node NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new1;
+	binary_tree_t *node1;
 
-	new1 = malloc(sizeof(binary_tree_t));
-	if (!new1)
+	node1 = malloc(sizeof(binary_tree_t));
+	if (!node1)
 		return (NULL);
-	new1->n = value;
-	new1->parent = parent;
-	new1->left = NULL;
-	new1->right = NULL;
-	return (new1);
+	node1->n = value;
+	node1->parent = parent;
+	node1->left = NULL;
+	node1->right = NULL;
+	return (node1);
+
 }
