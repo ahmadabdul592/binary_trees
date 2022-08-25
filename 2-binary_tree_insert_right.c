a4e0b1f8853ac8437510364392f5e976b24d009b
Abdul Ahmed Abdul <34511653+ahmadabdul592@users.noreply.github.com>
2022-08-25 17:36:48 +0100

    Binary Tree Implementation

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,38 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Inserts a node as the rigt child of another node
- * @parent: Pointer to the parent node of the node to insert the right child in
- * @value: Value to put in the new node
- * Return: Pointer to the new node else NULL
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ *
+ * Return: Pointer to the newly created node
+ *         NULL on failure
+ *         NULL if parent is NULL
  */
-
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
-	binary_tree_t *tmp = parent;
+	binary_tree_t *new;
 
-	if (parent == NULL)
-	{
+	if (!parent)
 		return (NULL);
-	}
-	if (new == NULL)
-	{
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
 		return (NULL);
-	}
 
 	new->n = value;
-	new->parent = tmp;
+	new->parent = parent;
 	new->left = NULL;
-	new->right = NULL;
-
-	if (tmp->right == NULL)
-		tmp->right = new;
-	else
-	{
-		new->right = parent->right;
-		parent->right = new;
+	new->right = parent->right;
+	parent->right = new;
+	if (new->right)
 		new->right->parent = new;
-	}
 	return (new);
 }
