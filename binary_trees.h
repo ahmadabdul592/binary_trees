ba9f29f7bbf58f48d412c1d67ba1bd9c8e8f0fa8
ahmadabdul592 <ahmadabdul592@gmail.com>
2022-08-26 15:12:24 -0700

    AVL Implementation

--- binary_trees.h
+++ binary_trees.h
@@ -87,6 +87,7 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
+
 /* Print Binary Tree */
 
 void binary_tree_print(const binary_tree_t *);
